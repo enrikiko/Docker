f3fca82e4b234c131aa62987083c50b80d02bd02
Enrique Ramos <enrikiko_91@hotmail.com>
2021-05-04 07:30:36 +0200

    Add flutter (#11)
    * Add Flutter image to dockerhub
    
    * Change build
    
    Co-authored-by: Enrique Ramos <enrique.ramos@gmail.com>

--- ExpressUploadImage/app.js
+++ ExpressUploadImage/app.js
@@ -5,14 +5,11 @@ const multer = require("multer");
 const express = require('express')
 const app = express()
 let count = 0
-// app.post('/test', function (req, res) {
-//   console.log(req);
-//   res.send('Hello World')
-// })
+
 const upload = multer({
   dest: "images/"
-  // you might also want to set some limits: https://github.com/expressjs/multer#limits
 });
+
 app.post('/test', upload.single("imageFile"),(req, res) => {
     // console.log(req.file.originalname);
     // console.log(req.file.path);
---
+++ Flutter/build.sh
@@ -0,0 +1,2 @@
+docker build -t enriqueramosmunoz/flutter:2.0.6 .
+docker push enriqueramosmunoz/flutter:2.0.6
